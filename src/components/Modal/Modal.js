584197cfa692b4e6325ea71a253265e68f0c293a
bondanjody <bondanjodys@gmail.com>
2023-04-07 00:38:05 +0700

    Mengatur animation timing

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,9 +3,19 @@ import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const Modal = (props) => {
   return (
-    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
